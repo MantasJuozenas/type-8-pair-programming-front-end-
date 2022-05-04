999e52db513e4e42d6a3681854c6265026d85a31
Mantas <m.juozenas@gmail.com>
2022-05-04 18:47:06 +0300

    style fix/medications.js done

---
+++ public/medication.js
@@ -0,0 +1,28 @@
+const sectionEl = document.querySelector('.med-grid');
+
+async function fetchData() {
+  const res = await fetch('http://localhost:3306/v1/medications');
+  const data = await res.json();
+  generateAll(data, sectionEl);
+}
+
+function generateCard(name, desc, dest) {
+  const divEl = document.createElement('div');
+  divEl.classList.add('card');
+  const h2El = document.createElement('h2');
+  h2El.textContent = name;
+  const pEl = document.createElement('p');
+  pEl.textContent = desc;
+  divEl.append(h2El, pEl);
+  dest.append(divEl);
+}
+
+function generateAll(arr, dest) {
+  dest.innerHTML = '';
+  arr.forEach((med) => {
+    console.log(med.name);
+    generateCard(med.name, med.description, dest);
+  });
+}
+
+fetchData();
